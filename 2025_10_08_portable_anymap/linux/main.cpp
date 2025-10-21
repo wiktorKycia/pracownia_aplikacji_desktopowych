1b9189b1a7717f3c9589296fbc991e051524410f
wiktor <wiktor.kycia@gmail.com>
2025-10-21 19:20:43 +0200

    implement class for PPM ASCII

--- 2025_10_08_portable_anymap/linux/main.cpp
+++ 2025_10_08_portable_anymap/linux/main.cpp
@@ -2,8 +2,57 @@
 
 using namespace std;
 
+/*
+PPM (P3)
+*/
+class PortablePixMapASCII
+{
+    static const int numberOfColors = {3};
+    unsigned int sizex, sizey;
+    u_int8_t ***pixels;
+    
+    public:
+    PortablePixMapASCII(unsigned int _sizex, unsigned int _sizey)
+    {
+        sizex = _sizex;
+        sizey = _sizey;
+        pixels = new u_int8_t**[sizey];
 
-class PortablePixMapASCII{};
+        for(unsigned int i = 0; i < sizey; i++)
+        {
+            pixels[i] = new u_int8_t*[sizex];
+
+            for(unsigned int j = 0; j < sizex; j++)
+            {
+                pixels[i][j] = new u_int8_t[numberOfColors]{0,0,0};
+            }
+        }
+    }
+    PortablePixMapASCII(const PortablePixMapASCII &obj)
+    {
+        sizex = obj.sizex;
+        sizey = obj.sizey;
+        pixels = new u_int8_t**[sizey];
+
+        for(unsigned int i = 0; i < sizey; i++)
+        {
+            pixels[i] = new u_int8_t*[sizex];
+
+            for(unsigned int j = 0; j < sizex; j++)
+            {
+                pixels[i][j] = new u_int8_t[numberOfColors]{
+                    obj.getPixel(i, j, 0),
+                    obj.getPixel(i, j, 1),
+                    obj.getPixel(i, j, 2)
+                };
+            }
+        }
+    }
+    u_int8_t getPixel(unsigned int x, unsigned int y, unsigned int color) const
+    {
+        return this->pixels[y][x][color];
+    }
+};
 class PortablePixMap{};
 class PortableGrayMapASCII{};
 class PortableGrayMap{};
