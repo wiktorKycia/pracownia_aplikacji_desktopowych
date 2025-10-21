039c78b753a4dac4c7181968d616a7880687ca90
wiktor <wiktor.kycia@gmail.com>
2025-10-21 19:01:17 +0200

    some changes idk

--- 2025_10_08_portable_anymap/linux/converter1/PortableAnymapConverter.cpp
+++ 2025_10_08_portable_anymap/linux/converter1/PortableAnymapConverter.cpp
@@ -1,40 +1,23 @@
 #include "PortableAnymapConverter.h"
 #include <iostream>
+#include <string>
+using namespace std;
 
-PortableAnymapConverter::PortableAnymapConverter(unsigned int _size_x, unsigned int _size_y, FileType _file_type)
+void PortableAnymapConverter::convertASCII(string fileName, FileType typeToSave)
 {
-    this->sizex = _size_x;
-    this->sizey = _size_y;
-    this->fileType = _file_type;
+    int dot_idx = fileName.find('.');
+    string name = fileName.substr(0, dot_idx); // to może się zepsuć
+    string fileExtension = fileName.substr(dot_idx); // to też bo nwm jak konkretnie działa substr
 
-    this->pixels = new u_int8_t*[sizey];
-
-    for(unsigned int i = 0; i < sizey; i++)
+    if(fileExtension == "ppm")
     {
-        this->pixels[i] = new u_int8_t[sizex];
-
-        for(unsigned int j = 0; j < sizex; j++)
+        if(typeToSave == FileType::PPM_ASCII)
         {
-            this->pixels[i][j] = portableAnymapConverter.pixels[i][j];
+            return;
         }
+        
     }
-};
 
-PortableAnymapConverter::PortableAnymapConverter(const PortableAnymapConverter& portableAnymapConverter)
-{
-    this->sizex = portableAnymapConverter.sizex;
-    this->sizey = portableAnymapConverter.sizey;
-    this->fileType = portableAnymapConverter.fileType;
 
-    this->pixels = new u_int8_t*[sizey];
 
-    for(unsigned int i = 0; i < sizey; i++)
-    {
-        this->pixels[i] = new u_int8_t[sizex];
-
-        for(unsigned int j = 0; j < sizex; j++)
-        {
-            this->pixels[i][j] = portableAnymapConverter.pixels[i][j];
-        }
-    }
-};
+}
--- 2025_10_08_portable_anymap/linux/converter1/PortableAnymapConverter.h
+++ 2025_10_08_portable_anymap/linux/converter1/PortableAnymapConverter.h
@@ -1,12 +1,8 @@
 #include "../FileType.h"
+#include<string>
 
 class PortableAnymapConverter
 {
-    unsigned int sizex, sizey;
-    FileType fileType;
-    u_int8_t **pixels;
-
     public:
-    PortableAnymapConverter(unsigned int _size_x, unsigned int _size_y, FileType _file_type);
-    PortableAnymapConverter(const PortableAnymapConverter& portableAnymapConverter);
+    static void convertASCII(std::string fileName, FileType typeToSave);
 };
