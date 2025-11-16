1f4d3d341dd37e769e6c3db001a71bfb33f6a1b3
wiktor <wiktor.kycia@gmail.com>
2025-11-16 19:13:38 +0100

    add cmake configuration and the built project

---
+++ cmake_tutorial/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wiktor/school/Technikum/Aplikacje desktopowe/zadania_z_lekcji/cmake_tutorial/hello.cpp" "CMakeFiles/main.dir/hello.cpp.o" "gcc" "CMakeFiles/main.dir/hello.cpp.o.d"
+  "/home/wiktor/school/Technikum/Aplikacje desktopowe/zadania_z_lekcji/cmake_tutorial/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
+  "" "main" "gcc" "CMakeFiles/main.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake_tutorial/build/CMakeFiles/main.dir/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/main.dir/main.cpp.o: \
+ /home/wiktor/school/Technikum/Aplikacje\ desktopowe/zadania_z_lekcji/cmake_tutorial/main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/wiktor/school/Technikum/Aplikacje\ desktopowe/zadania_z_lekcji/cmake_tutorial/hello.h
